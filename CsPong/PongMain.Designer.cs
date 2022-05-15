47b59a9f82afa58333fc303dd618206b2055cf35
Fuzz <50038407+TheFuzzProgrammer@users.noreply.github.com>
2022-05-14 23:31:24 -0300

    Agregar archivos de proyecto.

---
+++ CsPong/PongMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CsPong
+{
+    partial class PongMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeLabel = new System.Windows.Forms.Label();
+            this.minimizeLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.minimizeLabel);
+            this.panel1.Controls.Add(this.closeLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1024, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // closeLabel
+            // 
+            this.closeLabel.AutoSize = true;
+            this.closeLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeLabel.Location = new System.Drawing.Point(1000, 0);
+            this.closeLabel.Name = "closeLabel";
+            this.closeLabel.Size = new System.Drawing.Size(24, 25);
+            this.closeLabel.TabIndex = 0;
+            this.closeLabel.Text = "X";
+            this.closeLabel.Click += new System.EventHandler(this.closeLabel_Click);
+            // 
+            // minimizeLabel
+            // 
+            this.minimizeLabel.AutoSize = true;
+            this.minimizeLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.minimizeLabel.Location = new System.Drawing.Point(980, 0);
+            this.minimizeLabel.Name = "minimizeLabel";
+            this.minimizeLabel.Size = new System.Drawing.Size(20, 25);
+            this.minimizeLabel.TabIndex = 1;
+            this.minimizeLabel.Text = "-";
+            this.minimizeLabel.Click += new System.EventHandler(this.minimizeLabel_Click);
+            // 
+            // PongMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "PongMain";
+            this.Opacity = 0.96D;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label minimizeLabel;
+        private System.Windows.Forms.Label closeLabel;
+    }
+}
---
+++ CsPong/PongMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsPong
+{
+    public partial class PongMain : Form
+    {
+        public PongMain()
+        {
+            InitializeComponent();
+        }
+
+        private void closeLabel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void minimizeLabel_Click(object sender, EventArgs e)
+        {
+            if(this.WindowState != FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+    }
+}
